2aa2a04e9cf847d605e4a4417c2e79fceaf99bc8
Romany Refaat <romanyrefaat641l@gmail.com>
2026-09-11 11:51:03 +0300

    Ignored typescript build errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
