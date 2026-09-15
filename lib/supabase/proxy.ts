8074ad57cd413c8050fb5cae799916f69488e53b
Romany Refaat <romanyrefaat641l@gmail.com>
2026-09-15 13:48:38 +0300

    Supported jobs and /craftsman/find pages for non authenticated users

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -53,7 +53,9 @@ export async function updateSession(request: NextRequest) {
   !request.nextUrl.pathname.startsWith("/auth") &&
   !request.nextUrl.pathname.startsWith("/craftsman/auth/sign-up") &&
   !request.nextUrl.pathname.startsWith("/client/auth/sign-up") &&
-  !request.nextUrl.pathname.startsWith("/api/telegram")
+  !request.nextUrl.pathname.startsWith("/api/telegram") && 
+  !request.nextUrl.pathname.startsWith("/jobs") &&
+  !request.nextUrl.pathname.startsWith("/craftsman/find")
 ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
